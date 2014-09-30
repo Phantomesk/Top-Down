d9f708bbca80f798698317758a4668ee8605eb0e
Phantomesk <jacobmccy@gmail.com>
2014-09-29 17:55:50 -0700

    what the fuck

--- SHMUP/include/AIE.h
+++ SHMUP/include/AIE.h
@@ -60,81 +60,249 @@ typedef enum CameraDirection
 **/
 typedef enum
 {
-	MOUSE_BUTTON_1 = 0,
-	MOUSE_BUTTON_2 = 1,
-	MOUSE_BUTTON_3 = 2,
-	KEY_SPACE = 32,
-	KEY_SPECIAL = 256,
-	KEY_ESC = (KEY_SPECIAL + 1),
-	KEY_F1 = (KEY_SPECIAL + 2),
-	KEY_F2 = (KEY_SPECIAL + 3),
-	KEY_F3 = (KEY_SPECIAL + 4),
-	KEY_F4 = (KEY_SPECIAL + 5),
-	KEY_F5 = (KEY_SPECIAL + 6),
-	KEY_F6 = (KEY_SPECIAL + 7),
-	KEY_F7 = (KEY_SPECIAL + 8),
-	KEY_F8 = (KEY_SPECIAL + 9),
-	KEY_F9 = (KEY_SPECIAL + 10),
-	KEY_F10 = (KEY_SPECIAL + 11),
-	KEY_F11 = (KEY_SPECIAL + 12),
-	KEY_F12 = (KEY_SPECIAL + 13),
-	KEY_F13 = (KEY_SPECIAL + 14),
-	KEY_F14 = (KEY_SPECIAL + 15),
-	KEY_F15 = (KEY_SPECIAL + 16),
-	KEY_F16 = (KEY_SPECIAL + 17),
-	KEY_F17 = (KEY_SPECIAL + 18),
-	KEY_F18 = (KEY_SPECIAL + 19),
-	KEY_F19 = (KEY_SPECIAL + 20),
-	KEY_F20 = (KEY_SPECIAL + 21),
-	KEY_F21 = (KEY_SPECIAL + 22),
-	KEY_F22 = (KEY_SPECIAL + 23),
-	KEY_F23 = (KEY_SPECIAL + 24),
-	KEY_F24 = (KEY_SPECIAL + 25),
-	KEY_F25 = (KEY_SPECIAL + 26),
-	KEY_UP = (KEY_SPECIAL + 27),
-	KEY_DOWN = (KEY_SPECIAL + 28),
-	KEY_LEFT = (KEY_SPECIAL + 29),
-	KEY_RIGHT = (KEY_SPECIAL + 30),
-	KEY_LSHIFT = (KEY_SPECIAL + 31),
-	KEY_RSHIFT = (KEY_SPECIAL + 32),
-	KEY_LCTRL = (KEY_SPECIAL + 33),
-	KEY_RCTRL = (KEY_SPECIAL + 34),
-	KEY_LALT = (KEY_SPECIAL + 35),
-	KEY_RALT = (KEY_SPECIAL + 36),
-	KEY_TAB = (KEY_SPECIAL + 37),
-	KEY_ENTER = (KEY_SPECIAL + 38),
-	KEY_BACKSPACE = (KEY_SPECIAL + 39),
-	KEY_INSERT = (KEY_SPECIAL + 40),
-	KEY_DEL = (KEY_SPECIAL + 41),
-	KEY_PAGEUP = (KEY_SPECIAL + 42),
-	KEY_PAGEDOWN = (KEY_SPECIAL + 43),
-	KEY_HOME = (KEY_SPECIAL + 44),
-	KEY_END = (KEY_SPECIAL + 45),
-	KEY_KP_0 = (KEY_SPECIAL + 46),
-	KEY_KP_1 = (KEY_SPECIAL + 47),
-	KEY_KP_2 = (KEY_SPECIAL + 48),
-	KEY_KP_3 = (KEY_SPECIAL + 49),
-	KEY_KP_4 = (KEY_SPECIAL + 50),
-	KEY_KP_5 = (KEY_SPECIAL + 51),
-	KEY_KP_6 = (KEY_SPECIAL + 52),
-	KEY_KP_7 = (KEY_SPECIAL + 53),
-	KEY_KP_8 = (KEY_SPECIAL + 54),
-	KEY_KP_9 = (KEY_SPECIAL + 55),
-	KEY_KP_DIVIDE = (KEY_SPECIAL + 56),
-	KEY_KP_MULTIPLY = (KEY_SPECIAL + 57),
-	KEY_KP_SUBTRACT = (KEY_SPECIAL + 58),
-	KEY_KP_ADD = (KEY_SPECIAL + 59),
-	KEY_KP_DECIMAL = (KEY_SPECIAL + 60),
-	KEY_KP_EQUAL = (KEY_SPECIAL + 61),
-	KEY_KP_ENTER = (KEY_SPECIAL + 62),
-	KEY_KP_NUM_LOCK = (KEY_SPECIAL + 63),
-	KEY_CAPS_LOCK = (KEY_SPECIAL + 64),
-	KEY_SCROLL_LOCK = (KEY_SPECIAL + 65),
-	KEY_PAUSE = (KEY_SPECIAL + 66),
-	KEY_LSUPER = (KEY_SPECIAL + 67),
-	KEY_RSUPER = (KEY_SPECIAL + 68),
-	KEY_MENU = (KEY_SPECIAL + 69),
-	KEY_LAST = KEY_MENU
+#define 	GLFW_KEY_UNKNOWN   -1
+
+#define 	GLFW_KEY_SPACE   32
+
+#define 	GLFW_KEY_APOSTROPHE   39 /* ' */
+
+#define 	GLFW_KEY_COMMA   44 /* , */
+
+#define 	GLFW_KEY_MINUS   45 /* - */
+
+#define 	GLFW_KEY_PERIOD   46 /* . */
+
+#define 	GLFW_KEY_SLASH   47 /* / */
+
+#define 	GLFW_KEY_0   48
+
+#define 	GLFW_KEY_1   49
+
+#define 	GLFW_KEY_2   50
+
+#define 	GLFW_KEY_3   51
+
+#define 	GLFW_KEY_4   52
+
+#define 	GLFW_KEY_5   53
+
+#define 	GLFW_KEY_6   54
+
+#define 	GLFW_KEY_7   55
+
+#define 	GLFW_KEY_8   56
+
+#define 	GLFW_KEY_9   57
+
+#define 	GLFW_KEY_SEMICOLON   59 /* ; */
+
+#define 	GLFW_KEY_EQUAL   61 /* = */
+
+#define 	GLFW_KEY_A   65
+
+#define 	GLFW_KEY_B   66
+
+#define 	GLFW_KEY_C   67
+
+#define 	GLFW_KEY_D   68
+
+#define 	GLFW_KEY_E   69
+
+#define 	GLFW_KEY_F   70
+
+#define 	GLFW_KEY_G   71
+
+#define 	GLFW_KEY_H   72
+
+#define 	GLFW_KEY_I   73
+
+#define 	GLFW_KEY_J   74
+
+#define 	GLFW_KEY_K   75
+
+#define 	GLFW_KEY_L   76
+
+#define 	GLFW_KEY_M   77
+
+#define 	GLFW_KEY_N   78
+
+#define 	GLFW_KEY_O   79
+
+#define 	GLFW_KEY_P   80
+
+#define 	GLFW_KEY_Q   81
+
+#define 	GLFW_KEY_R   82
+
+#define 	GLFW_KEY_S   83
+
+#define 	GLFW_KEY_T   84
+
+#define 	GLFW_KEY_U   85
+
+#define 	GLFW_KEY_V   86
+
+#define 	GLFW_KEY_W   87
+
+#define 	GLFW_KEY_X   88
+
+#define 	GLFW_KEY_Y   89
+
+#define 	GLFW_KEY_Z   90
+
+#define 	GLFW_KEY_LEFT_BRACKET   91 /* [ */
+
+#define 	GLFW_KEY_BACKSLASH   92 /* \ */
+
+#define 	GLFW_KEY_RIGHT_BRACKET   93 /* ] */
+
+#define 	GLFW_KEY_GRAVE_ACCENT   96 /* ` */
+
+#define 	GLFW_KEY_WORLD_1   161 /* non-US #1 */
+
+#define 	GLFW_KEY_WORLD_2   162 /* non-US #2 */
+
+#define 	GLFW_KEY_ESCAPE   256
+
+#define 	GLFW_KEY_ENTER   257
+
+#define 	GLFW_KEY_TAB   258
+
+#define 	GLFW_KEY_BACKSPACE   259
+
+#define 	GLFW_KEY_INSERT   260
+
+#define 	GLFW_KEY_DELETE   261
+
+#define 	GLFW_KEY_RIGHT   262
+
+#define 	GLFW_KEY_LEFT   263
+
+#define 	GLFW_KEY_DOWN   264
+
+#define 	GLFW_KEY_UP   265
+
+#define 	GLFW_KEY_PAGE_UP   266
+
+#define 	GLFW_KEY_PAGE_DOWN   267
+
+#define 	GLFW_KEY_HOME   268
+
+#define 	GLFW_KEY_END   269
+
+#define 	GLFW_KEY_CAPS_LOCK   280
+
+#define 	GLFW_KEY_SCROLL_LOCK   281
+
+#define 	GLFW_KEY_NUM_LOCK   282
+
+#define 	GLFW_KEY_PRINT_SCREEN   283
+
+#define 	GLFW_KEY_PAUSE   284
+
+#define 	GLFW_KEY_F1   290
+
+#define 	GLFW_KEY_F2   291
+
+#define 	GLFW_KEY_F3   292
+
+#define 	GLFW_KEY_F4   293
+
+#define 	GLFW_KEY_F5   294
+
+#define 	GLFW_KEY_F6   295
+
+#define 	GLFW_KEY_F7   296
+
+#define 	GLFW_KEY_F8   297
+
+#define 	GLFW_KEY_F9   298
+
+#define 	GLFW_KEY_F10   299
+
+#define 	GLFW_KEY_F11   300
+
+#define 	GLFW_KEY_F12   301
+
+#define 	GLFW_KEY_F13   302
+
+#define 	GLFW_KEY_F14   303
+
+#define 	GLFW_KEY_F15   304
+
+#define 	GLFW_KEY_F16   305
+
+#define 	GLFW_KEY_F17   306
+
+#define 	GLFW_KEY_F18   307
+
+#define 	GLFW_KEY_F19   308
+
+#define 	GLFW_KEY_F20   309
+
+#define 	GLFW_KEY_F21   310
+
+#define 	GLFW_KEY_F22   311
+
+#define 	GLFW_KEY_F23   312
+
+#define 	GLFW_KEY_F24   313
+
+#define 	GLFW_KEY_F25   314
+
+#define 	GLFW_KEY_KP_0   320
+
+#define 	GLFW_KEY_KP_1   321
+
+#define 	GLFW_KEY_KP_2   322
+
+#define 	GLFW_KEY_KP_3   323
+
+#define 	GLFW_KEY_KP_4   324
+
+#define 	GLFW_KEY_KP_5   325
+
+#define 	GLFW_KEY_KP_6   326
+
+#define 	GLFW_KEY_KP_7   327
+
+#define 	GLFW_KEY_KP_8   328
+
+#define 	GLFW_KEY_KP_9   329
+
+#define 	GLFW_KEY_KP_DECIMAL   330
+
+#define 	GLFW_KEY_KP_DIVIDE   331
+
+#define 	GLFW_KEY_KP_MULTIPLY   332
+
+#define 	GLFW_KEY_KP_SUBTRACT   333
+
+#define 	GLFW_KEY_KP_ADD   334
+
+#define 	GLFW_KEY_KP_ENTER   335
+
+#define 	GLFW_KEY_KP_EQUAL   336
+
+#define 	GLFW_KEY_LEFT_SHIFT   340
+
+#define 	GLFW_KEY_LEFT_CONTROL   341
+
+#define 	GLFW_KEY_LEFT_ALT   342
+
+#define 	GLFW_KEY_LEFT_SUPER   343
+
+#define 	GLFW_KEY_RIGHT_SHIFT   344
+
+#define 	GLFW_KEY_RIGHT_CONTROL   345
+
+#define 	GLFW_KEY_RIGHT_ALT   346
+
+#define 	GLFW_KEY_RIGHT_SUPER   347
+
+#define 	GLFW_KEY_MENU   348
+
+#define 	GLFW_KEY_LAST   GLFW_KEY_MENU
 };
 
 /**
--- SHMUP/source/main.cpp
+++ SHMUP/source/main.cpp
@@ -1,27 +1,236 @@
 ﻿#include "AIE.h"
 #include <iostream>
 
+const int iScreenHeight = 850;
+const int iScreenWidth = 700;
+
+char playerScore1[10];
+char highScore1[10];
+
+int playerScore = 000000000;
+int highScore = 000000000;
+
+enum GAMESTATES
+{
+	MAIN_MENU,
+	GAMEPLAY,
+	HIGHSCORE,
+	GAMEOVER,	//the different gamestates of my game
+	EXIT_GAME
+};
+GAMESTATES eCurrentState = MAIN_MENU;
+
+struct Player
+{
+	unsigned int iPlaneID;
+	float x;
+	float y;
+	float ySpeed = 250.f;
+	float xSpeed = 250.f;
+	float width;
+	float height;
+	float upperBoundry = y + height*.5f;
+	float lowerBoundry = y - height*.5f;
+	float rightBoundry = x + width*.5f;
+	float leftBoundry = x - width*.5f;
+	unsigned int iMoveUpKey;
+	unsigned int iMoveDownKey;
+	unsigned int iMoveRightKey;
+	unsigned int iMoveLeftKey;
+
+	void SetSize(float a_width, float a_height)
+	{
+		width = a_width;
+		height = a_height;
+	}
+
+	void SetPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+
+	void Movement(float a_fDeltaTime)
+	{
+		if (IsKeyDown(GLFW_KEY_UP))
+		{
+			y += ySpeed * a_fDeltaTime;;
+			if (y > (iScreenHeight -(height*.5f)))
+			{
+				y = iScreenHeight - (height*.5f);
+			}
+		}
+		if (IsKeyDown(GLFW_KEY_DOWN))
+		{
+			y -= ySpeed * a_fDeltaTime;
+			if (y < height*.5f)
+			{
+				y = height*.5f;
+			}
+		}
+		if (IsKeyDown(GLFW_KEY_LEFT))
+		{
+			x -= xSpeed * a_fDeltaTime;
+			if (x < (0+(width*.5f)))
+			{
+				x = 0 + (width *.5f);
+			}
+		}
+		if (IsKeyDown(GLFW_KEY_RIGHT))
+		{
+			x += xSpeed * a_fDeltaTime;
+			if (x > (iScreenWidth - (width * .5f)))
+			{
+				x = iScreenWidth - (width*.5f);
+			}
+		}
+		MoveSprite(iPlaneID, x, y);
+	}
+};
+Player playerPlane;
+
+void UpdateMainMenu();
+void UpdateGamePlay();
+void UpdateHighScore();
+void UpdateGameOverScreen();
+
 int main( int argc, char* argv[] )
 {	
-    Initialise(800, 600, false, "My Awesome Game");
+    Initialise(iScreenWidth, iScreenHeight, false, "Top-Down");
     
     SetBackgroundColour(SColour(0, 0, 0, 255));
 
-    int myTextureHandle = CreateSprite("./images/crate_sideup.png", 64, 64, true);
-    float xPos = 100;
-    float yPos = 100;
+	playerPlane.SetSize(75.f, 64.f);
+
+	playerPlane.SetPosition(iScreenWidth * .5f, 150.f);
+
+	playerPlane.iPlaneID = CreateSprite("./images/PlayerPlane.png", playerPlane.width, playerPlane.height, true);
 
     //Game Loop
     do
     {
-        MoveSprite(myTextureHandle, xPos, yPos);
-        DrawSprite(myTextureHandle);
+		switch (eCurrentState)
+		{
+		case MAIN_MENU:
+			UpdateMainMenu();
+			break;
 
-        ClearScreen();
+		case GAMEPLAY:
+			UpdateGamePlay();
+			break;
+
+		case HIGHSCORE:
+			UpdateHighScore();
+			break;
+
+		case GAMEOVER:
+			UpdateGameOverScreen();
+			break;
+
+		case EXIT_GAME:
+			return 0;
+
+		default:
+			break;
+		}
+		ClearScreen();
 
     } while(!FrameworkUpdate());
 
     Shutdown();
 
     return 0;
+}
+
+void UpdateMainMenu()
+{
+	DrawString("Welcome to SHMUP", iScreenWidth *.35f, iScreenHeight *.7f);
+	DrawString("Press Enter to start.", iScreenWidth*.35f, iScreenHeight*.6f);
+	DrawString("Press H for High Score", iScreenWidth * .35f, iScreenHeight * .5f);
+	DrawString("Press Esc to exit.", iScreenWidth*.35f, iScreenHeight*.4f);
+	if (IsKeyDown(GLFW_KEY_ENTER))
+	{
+		eCurrentState = GAMEPLAY;
+		ClearScreen();
+	}
+	if (IsKeyDown('H'))
+	{
+		eCurrentState = HIGHSCORE;
+		ClearScreen();
+	}
+	if (IsKeyDown(GLFW_KEY_ESCAPE))
+	{
+		eCurrentState = EXIT_GAME;
+	}
+
+	ClearScreen();
+}
+
+void UpdateGamePlay()
+{
+	float fDeltaTime = GetDeltaTime();
+
+	playerPlane.Movement(fDeltaTime);
+
+	MoveSprite(playerPlane.iPlaneID, playerPlane.x, playerPlane.y);
+
+	DrawSprite(playerPlane.iPlaneID);
+
+	DrawString(playerScore1, iScreenWidth*.975f, iScreenHeight - 2);
+
+	if (IsKeyDown('E'))
+	{
+		eCurrentState = MAIN_MENU;
+		ClearScreen();
+	}
+
+	itoa(playerScore, playerScore1, 10);
+
+	ClearScreen();
+}
+
+void UpdateHighScore()
+{
+	DrawString("Player :", iScreenWidth*.35f, iScreenHeight*.55f);
+	DrawString(playerScore1, iScreenWidth*.5f, iScreenHeight*.55f);
+	DrawString("Press E to go to Main Menu", iScreenWidth*.025, iScreenHeight - 2);
+	DrawString("Press Enter to start game", iScreenWidth* .6f, iScreenHeight - 2);
+
+	if (IsKeyDown('E'))
+	{
+		eCurrentState = MAIN_MENU;
+		ClearScreen();
+	}
+	if (IsKeyDown(GLFW_KEY_ENTER))
+	{
+		playerScore = 00000000;
+
+		eCurrentState = GAMEPLAY;
+
+		ClearScreen();
+	}
+	ClearScreen();
+}
+
+void UpdateGameOverScreen()
+{
+	DrawString("You were rekt bitch", iScreenWidth*.35f, iScreenHeight * .7f);
+	DrawString("Press Enter to start over", iScreenWidth*.35f, iScreenHeight*.6f);
+	DrawString("Press E to go to Main Menu", iScreenWidth*.35f, iScreenHeight*.5f);
+	DrawString("Press H for High Score", iScreenWidth * .35f, iScreenHeight * .4f);
+
+	if (IsKeyDown(GLFW_KEY_ENTER))
+	{
+		eCurrentState = GAMEPLAY;
+	}
+	if (IsKeyDown('E'))
+	{
+		eCurrentState = MAIN_MENU;
+	}
+	if (IsKeyDown('H'))
+	{
+		eCurrentState = HIGHSCORE;
+	}
+
+	ClearScreen();
 }
